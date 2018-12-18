8fda21f71067dce7c46d8ac6502badb0136f090b
Hugo Nogueira <hugomn@gmail.com>
2018-12-18 20:02:58 +0100

    Initial version

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new signature generator.</p>
+  </Layout>
+)
+
+export default IndexPage
